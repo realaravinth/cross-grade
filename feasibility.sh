be6ef90a75603808196bd6926cde4f0cd5908607
realaravinth <aravinth7820@gmail.com>
2020-03-30 18:52:44 +0530

    fixed feasibility.sh

--- feasibility.sh
+++ feasibility.sh
@@ -6,16 +6,21 @@ source $cgrade_root/env_vars.sh
 
 start_time=$(date +"%F_%H-%M")
 
+echo "[*] Getting installed packages"
+dpkg -l | grep ^ii | sed 's_  _\t_g' | 
+cut -f 2 > $cgrade_backup/pkg-list-$start_time.txt
 
-dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > $cgrade_backup/pkg-list-$start_time.txt
-cat $cgrade_backup/pkg-list-$start_time.txt
+echo "[*] Getting size occupied by installed packages"
 while read line 
 do
-    apt-cache show $line | grep "Installed-Size" | cut -d ' ' -f 2 >> $cgrade_tmp_runtime-$start_time.txt
+    apt-cache show $line |
+    grep "Installed-Size" |
+     cut -d ' ' -f 2 >> $cgrade_tmp_runtime-$start_time-sizes.txt
 done < $cgrade_backup/pkg-list-$start_time.txt
 
+echo "[*] Checking for disk space availability"
 available_size=$(df / | grep overlay | cut -d ' ' -f 11)
-status=$(python3 $cgrade_root/space-check.py $cgrade_tmp_runtime-$start_time.txt $available_size)
+status=$(python3 $cgrade_root/space-check.py $cgrade_tmp_runtime-$start_time-sizes.txt $available_size)
 
 if [ $status -eq 1 ]
 then    
@@ -24,4 +29,4 @@ exit
 fi
 
 echo "Sufficient disk space available, proceed with cross-grade"
-
+rm $cgrade_tmp_runtime-$start_time-sizes.txt
